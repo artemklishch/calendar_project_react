b0f733a8ae02ba2f8392dad5a4b2fc5d8efc06ad
Artem <artemklishch@gmail.com>
2020-02-18 17:12:08 +0200

    dfg

--- src/HeaderElems.jsx
+++ src/HeaderElems.jsx
@@ -1,69 +1,97 @@
-import React, { PureComponent } from 'react' ;
+import React, { PureComponent } from 'react';
 import WeekDays from './WeekDays';
 import NavigationElems from './NavigationElems';
+import moment from 'moment';
 
 
 class HeaderElems extends PureComponent {
   state = {
-    arrDaysOfWeek: []
+    arrDaysOfWeek: [],
+    counter: 0
   };
 
-  componentDidMount(){
-    this.generateArrDaysOfWeek();
+  componentDidMount() {
+    this.generateCurrentWeek();
+    this.renderTitleDate(this.state.arrDaysOfWeek[0], this.state.arrDaysOfWeek[6]);
   }
 
-  generateArrDaysOfWeek = () => {
-    const currentFullDate = new Date();//full day
-    const currentDate = currentFullDate.getDate();//date
-    const currentDayOfWeek = currentFullDate.getDay();//day num
+  componentDidUpdate(){
+    this.renderTitleDate(this.state.arrDaysOfWeek[0], this.state.arrDaysOfWeek[6]);
+  }
 
-    let countToDown = 0;
-    let countToUp = 0;
-    Array(7).fill(null)
-        .forEach((element,index) => {
-            if(index < currentDayOfWeek){
-                countToDown++;
-            }else if(index > currentDayOfWeek){
-                countToUp++;
-            }
-            // if(currentDayOfWeek === index){
-            //     let tempElem = element.closest('.header__week-block_days');
-            //     tempElem.classList.add('today__header__week-block_days');
-            // }
-        });
-    
-    countToDown = countToDown * 24 * 60 * 60 * 1000;
-    countToUp = countToUp * 24 * 60 * 60 * 1000; 
-    let milliSeconds = 0;
-     const arrDaysOfWeek = Array(7).fill(null)
+  generateCurrentWeek = () => {
+    const firstDayOfWeek = new Date();
+    while (firstDayOfWeek.getDay() !== 0) {
+      firstDayOfWeek.setDate(firstDayOfWeek.getDate() - 1);
+    }
+    const arrDaysOfWeek = Array(7).fill(null)
       .map(elem => {
-        elem = new Date(Date.now() - countToDown + milliSeconds);
-        milliSeconds += 86400000;
+        elem = new Date(firstDayOfWeek);
+        firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 1);
         return elem;
       });
     this.setState({
       arrDaysOfWeek,
+      counter: 0,
+      dateTitle: ''
     });
-};
+  };
 
-  render(){
-    // const weekDays = [
-    //   "Sun", "Mon", "Tue", "Wen", "Th", "Fr", "Sat"
-    // ];
+  generateAnotherWeek = event => {
+    const checkArrow = event.target.classList.contains('nav__arow_left') ||
+      event.target.classList.contains('nav__arow_right');
+    if (!checkArrow) return;
+
+    const arr = this.state.arrDaysOfWeek.map(date =>
+      event.target.classList.contains('nav__arow_right')
+        ? new Date(date.setDate(date.getDate() + 7))
+        : new Date(date.setDate(date.getDate() - 7)));
+
+    this.setState({
+      arrDaysOfWeek: arr,
+      counter: event.target.classList.contains('nav__arow_right')
+        ? this.state.counter + 1 : this.state.counter - 1
+    });
+  };
+
+  renderTitleDate = (firstDayOfWeek, lastDayOfWeek) => {
+    const firstDayMonth = moment(firstDayOfWeek).format('MMM');
+    const firstDayYear = moment(firstDayOfWeek).format('YYYY');
+
+    const lastDayMonth = moment(lastDayOfWeek).format('MMM');
+    const lastDayYear = moment(lastDayOfWeek).format('YYYY');
+
+    const dateTitle = firstDayMonth === lastDayMonth && firstDayYear === lastDayYear
+      ? `${firstDayMonth} ${firstDayYear}`
+      : firstDayMonth !== lastDayMonth && firstDayYear === lastDayYear
+        ? `${firstDayMonth} - ${lastDayMonth.toLocaleLowerCase()} ${firstDayYear}`
+        : firstDayMonth !== lastDayMonth && firstDayYear !== lastDayYear
+          ? `${firstDayMonth} ${firstDayYear} - ${lastDayMonth} ${lastDayYear}`
+          : '';
+
+    this.setState({ dateTitle });
+  };
+
+  render() {
     return (
       <header className="header">
-        <NavigationElems />
+        <NavigationElems
+          onClickAnotherWeek={this.generateAnotherWeek}
+          onClickTodayWeek={this.generateCurrentWeek}
+          dateTitle={this.state.dateTitle}
+        />
         <div className="header__week-block">
           <div className="header__week-block_gmt-cell">
             gmt+02
             <div className="header__week-block_gmt-cell_part-bord"></div>
           </div>
-  
+
           <div className="header__week-block_line">
-           {this.state.arrDaysOfWeek.map(elem => <WeekDays 
-              key={elem}
-              dayDate={elem}
-            />)
+            {
+              this.state.arrDaysOfWeek.map(dateObject => <WeekDays
+                key={dateObject}
+                dayDate={dateObject}
+              />)
             }
           </div>
         </div>
--- src/NavigationElems.jsx
+++ src/NavigationElems.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const NavigationElems = () => {
+const NavigationElems = ({ onClickAnotherWeek, onClickTodayWeek, dateTitle }) => {
   return (
     <nav className="nav">
       <button className="nav__button">
         <img src="https://img.icons8.com/color/48/000000/plus-math.png" className="plus" alt="Plus img" />
         Create
         </button>
-      <button className="nav_day">Today</button>
-      <div className="nav__arow">
+      <button className="nav_day" onClick={onClickTodayWeek}>Today</button>
+      <div className="nav__arow" onClick={onClickAnotherWeek}>
         <div className="nav__arow_left"></div>
         <div className="nav__arow_right"></div>
       </div>
       <div className="nav__dateMonEar">
-        <h2 className="nav__dateMonEar-today"> Dec-2019 </h2>
+        <h2 className="nav__dateMonEar-today">{dateTitle}</h2>
       </div>
     </nav>
   );
