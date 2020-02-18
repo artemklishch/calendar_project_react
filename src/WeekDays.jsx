a0e2bbab65b109379fdb4c5bd37b76a4178ffdef
Artem <artemklishch@gmail.com>
2020-02-18 14:29:27 +0200

    h

--- src/WeekDays.jsx
+++ src/WeekDays.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Moment from 'moment';
+import moment from 'moment';
 
 const WeekDays = ({ elem }) => {
-  
+  const day = moment(elem).format('ddd');
+  const date = moment(elem).format("D");
   return (
     <div className="header__week-block_days">
       <div className="header__week-block_part-bord"></div>
-      <p className="header__week-block_dayname">{elem}</p>
-      <p className="header__week-block_daydate"></p>
+      <p className="header__week-block_dayname">{day}</p>
+      <p className="header__week-block_daydate">{date}</p>
     </div>
   );
 };
