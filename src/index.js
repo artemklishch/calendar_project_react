b7b218cacb8d728d081cea33a29c21e171902364
Artem <artemklishch@gmail.com>
2020-02-18 10:42:36 +0200

    fdg

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import './index.scss';
+import './common.scss';
+import './main.scss';
+import './header.scss';
+import './popup.scss';
+import './redLine.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<App />, rootElement);
