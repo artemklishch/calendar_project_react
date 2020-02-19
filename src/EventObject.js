c3a90c0f494049eab0d2f7a28f63a4712a2eeb37
Artem <artemklishch@gmail.com>
2020-02-19 15:59:23 +0200

    sdf

--- src/EventObject.js
+++ src/EventObject.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EventObject = ({objectElem}) => {
   return (
-    <div className='main__sidebar_day_object' >
+    <div className='main__sidebar_day_object' style={{height: '150%'}} >
       <h4>{objectElem.header}</h4>
       <p></p>
     </div>
