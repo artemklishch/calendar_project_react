7ff10feb533c92df59700fb4cc3cea74fa196d54
Artem <artemklishch@gmail.com>
2020-04-08 12:31:41 +0300

    про

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,14 +4,22 @@ import App from '../App';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
- 
 Enzyme.configure({ adapter: new Adapter() });
 
 
 describe('App', () => {
   it ('should render main elements', () => {
     const wrappedComponent = shallow(<App />);
-    expect(wrappedComponent.find('Login').exists(true));
+    expect(wrappedComponent.find('HeaderElems').exists()).toBeTruthy();
+  })
+
+  it ('should render main elements', () => {
+    const wrappedComponent = shallow(<App />);
+    expect(wrappedComponent.find('MainBars').exists()).toBeTruthy();
+  })
+
+  it ('should render main elements', () => {
+    const wrappedComponent = shallow(<App />);
+    expect(wrappedComponent.find('ModalWindow').exists()).toBeTruthy();
   })
 });
