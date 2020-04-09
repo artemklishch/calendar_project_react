48995ca7a501c31daeec0d21931d1a74537af2b4
Artem <artemklishch@gmail.com>
2020-04-09 19:00:27 +0300

    fds

--- src/modalWindow/ModalWindow.test.jsx
+++ src/modalWindow/ModalWindow.test.jsx
@@ -31,4 +31,56 @@ describe('ModalWindow', () => {
     const wrappedComponent = shallow(<ModalWindow {...props} />);
     expect(toJson(wrappedComponent)).toMatchSnapshot();
   })
+
+  it('should call function on submit', () => {
+    const props = {
+      isOpen: true,
+      onHideForm: jest.fn(),
+      onDeleteEvent: jest.fn(),
+      onValidate: jest.fn(),
+      validateText: 'text 1',
+      backgroundColor: 'red',
+      tempObj: {},
+      onCreateEvent: jest.fn(),
+      onLateEditing: false,
+    };
+    const wrappedComponent = shallow(<ModalWindow {...props} />);
+    wrappedComponent.find('.popup').simulate('submit');
+    expect(props.onCreateEvent).toBeCalled();
+  })
+
+  it('should call function on close', () => {
+    const props = {
+      isOpen: true,
+      onHideForm: jest.fn(),
+      onDeleteEvent: jest.fn(),
+      onValidate: jest.fn(),
+      validateText: 'text 1',
+      backgroundColor: 'red',
+      tempObj: {},
+      onCreateEvent: jest.fn(),
+      onLateEditing: false,
+    };
+    const wrappedComponent = shallow(<ModalWindow {...props} />);
+    wrappedComponent.find('.popup__btn-close').simulate('click');
+    expect(props.onHideForm).toBeCalled();
+  })
+
+
+  it('should call function on delete', () => {
+    const props = {
+      isOpen: true,
+      onHideForm: jest.fn(),
+      onDeleteEvent: jest.fn(),
+      onValidate: jest.fn(),
+      validateText: 'text 1',
+      backgroundColor: 'red',
+      tempObj: {},
+      onCreateEvent: jest.fn(),
+      onLateEditing: false,
+    };
+    const wrappedComponent = shallow(<ModalWindow {...props} />);
+    wrappedComponent.find('#delete-img').simulate('click');
+    expect(props.onDeleteEvent).toBeCalled();
+  })
 });
